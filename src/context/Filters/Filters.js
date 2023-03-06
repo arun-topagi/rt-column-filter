50cb31fda0c65ee6d2d388c62b458ae9a4be307c
Arunkumar S Topagi <arunkumartopagi@gmail.com>
2023-03-06 08:51:46 +0530

    remove underscore

--- src/context/Filters/Filters.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react'
-
-const initialValue = {}
-
-const Filters = createContext(initialValue)
-
-export const useFilters = () => useContext(Filters)
-
-export default Filters
